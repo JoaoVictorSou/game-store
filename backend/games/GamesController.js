21c8a5af0d094f7063d63411fd3457c30b43fc51
João Victor <soulima.joao@gmail.com>
2021-11-27 13:52:53 -0300

    uso do método sendstatus

--- backend/games/GamesController.js
+++ backend/games/GamesController.js
@@ -11,14 +11,12 @@ router.get('/games', (req, res) => {
                 res.statusCode = 200
                 res.json(games)
             } else {
-                res.statusCode = 404
-                res.send('[ERR] Not found: 404')
+                res.sendStatus(404)
             }
         })
         .catch(err => {
             console.log(`[ERR] FIND ALL GAMES: ${err}`)
-            res.statusCode = 500
-            res.send(`[ERR] SERVER ERROR: 500`)
+            res.sendStatus(500)
         })
 
 })
@@ -34,18 +32,15 @@ router.get('/game/:id', (req, res) => {
                     res.statusCode = 200
                     res.json(game)
                 } else {
-                    res.statusCode = 404
-                    res.send(404)
+                    res.sendStatus(404)
                 }
             })
             .catch(err => {
                 console.log(`[ERR] FIND ESPECIFIC GAME: ${err}`)
-                res.statusCode = 500
-                res.send(500)
+                res.sendStatus(500)
             })
     } else {
-        res.statusCode = 400
-        res.send(400)
+        res.sendStatus(400)
     }
 
 })
@@ -55,21 +50,24 @@ router.post('/game', (req, res) => {
     const year = parseInt(req.body.year)
     const price = parseFloat(req.body.price).toFixed(2)
 
-    Game
-        .create({
-            title,
-            year,
-            price
-        })
-        .then(_ => {
-            res.statusCode = 201
-            res.send(`[SUC] CREATE GAME AT TABLE: 201`)
-        })
-        .catch(_ => {
-            console.log(`[ERR] CREATE GAME AT TABLE: ${err}`)
-            res.statusCode = 500
-            res.send(`[ERR] CREATE GAME AT TABLE: 500`)
-        })
+    if (title && year && price) {
+        Game
+            .create({
+                title,
+                year,
+                price
+            })
+            .then(_ => {
+                res.sendStatus(201)
+            })
+            .catch(_ => {
+                console.log(`[ERR] CREATE GAME AT TABLE: ${err}`)
+                res.sendStatus(500)
+            })
+    } else {
+        res.statusCode = 400
+        res.send(400)
+    }
 })
 
 module.exports = router

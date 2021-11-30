ca115d5d2120e66a8209dbc7121344351d9bd4ba
João Victor <soulima.joao@gmail.com>
2021-11-30 12:48:10 -0300

    token de autentifição

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const connection = require('./database/connection')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const connection = require('./database/connection')
 
 //MODEL IMPORTS
 const Game = require('./games/Game')
@@ -13,6 +15,7 @@ const usersController = require('./users/UsersController')
 
 //SERVER SETTINGS
 const app = express()
+const JWTSecret = '552210'
 app.use(cors())
 app.use(bodyParser.urlencoded( {extended: false} ))
 app.use(bodyParser.json())
@@ -28,6 +31,51 @@ connection
     })
 
 //ROUTES
+app.post("/auth", (req, res) => {
+    const {email, password} = req.body
+
+    if(email && password) {
+        User
+            .findOne({
+                where: {
+                    email: email
+                }
+            })
+            .then(user => {
+                if (user) {
+                    let hash = user.password
+                    let isCorrect = bcrypt.compareSync(password, hash)
+
+                    if (isCorrect) {
+                        jwt.sign({
+                            name: user.name,
+                            email: user.email,
+                            id: user.id
+                        }, JWTSecret,
+                        {
+                            expiresIn: '1h'
+                        }, (err, token) => {
+                            if (err) {
+                                console.log(`[ERR] USER AUTHENTICATE: ${err}`)
+                                res.sendStatus(500)
+                            } else {
+                                res.status(200)
+                                res.json({ token })
+                            }
+                        })
+                    } else {
+                        res.sendStatus(401)
+                    }
+                } else {
+                    res.sendStatus(404)
+                }
+            })
+    } else {
+        res.sendStatus(400)
+    }
+})
+
+//DOMANIS ROUTES
 app.use('/', gamesController)
 app.use('/', usersController)
 
